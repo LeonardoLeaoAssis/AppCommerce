ae8b9e69455400982616380ab0531e14fcd71670
Leonardo Leão de Assis <leonardo.leao.assis.gitkraken@gmail.com>
2021-02-23 11:58:01 -0300

    commit

--- app/src/main/java/br/com/arquitetoandroid/appcommerce/CartFragment.kt
+++ app/src/main/java/br/com/arquitetoandroid/appcommerce/CartFragment.kt
@@ -5,31 +5,62 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import br.com.arquitetoandroid.appcommerce.adapter.ProductAdapter
-import br.com.arquitetoandroid.appcommerce.model.Order
+import br.com.arquitetoandroid.appcommerce.adapter.CartAdapter
+import br.com.arquitetoandroid.appcommerce.model.*
 
 class CartFragment: Fragment() {
 
     lateinit var recyclerCart: RecyclerView
     lateinit var order: Order
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-//        if (arguments != null) {
-//            arrayProduct = (arguments?.getSerializable("CATEGORY") as ProductCategory).products
-//        }
-    }
-
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view: View = inflater.inflate(R.layout.fragment_cart, container, false)
 
         recyclerCart = view.findViewById(R.id.rv_cart)
-        val adapterProdutc: ProductAdapter = ProductAdapter(arrayListOf(), requireContext())
-        recyclerCart.adapter = adapterProdutc
+
+        val colors: List<ProductColor> = arrayListOf(
+            ProductColor("1", "Vermelho", "#FF0000"),
+            ProductColor("2", "Amarelo", "#FFFF00"),
+            ProductColor("3", "Verde", "#008000"),
+            ProductColor("4", "Azul", "#0000FF"),
+            ProductColor("5", "Cinza", "#444444")
+        )
+
+        val sizes: List<ProductSize> = arrayListOf<ProductSize>(
+            ProductSize("1", "P"),
+            ProductSize("2", "M"),
+            ProductSize("3", "G")
+        )
+
+        val product1: Product = Product(
+            "1",
+            "Camiseta 1",
+            ProductCategory("1", "Camisetas"),
+            "Vocẽ selecionou a camiseta 1",
+            1.00,
+            colors,
+            sizes,
+            arrayListOf<ProductImage>(ProductImage("1", "xxx")))
+
+        val product2: Product = Product(
+            "2",
+            "Camiseta 2",
+            ProductCategory("1", "Camisetas"),
+            "Vocẽ selecionou a camiseta 2",
+            2.00,
+            colors,
+            sizes,
+            arrayListOf<ProductImage>(ProductImage("1", "xxx")))
+
+        val adapterCart: CartAdapter = CartAdapter(arrayListOf(OrderedProduct("1", product1, 1),
+                                                               OrderedProduct("2", product2, 2),
+                                                               OrderedProduct("3", product1, 3),
+                                                               OrderedProduct("4", product2, 4)),
+                requireContext())
+
+        recyclerCart.adapter = adapterCart
         recyclerCart.layoutManager = LinearLayoutManager(requireContext(), RecyclerView.VERTICAL, false)
 
         return view
--- app/src/main/java/br/com/arquitetoandroid/appcommerce/adapter/CartAdapter.kt
+++ app/src/main/java/br/com/arquitetoandroid/appcommerce/adapter/CartAdapter.kt
@@ -1,24 +1,28 @@
 package br.com.arquitetoandroid.appcommerce.adapter
 
 import android.content.Context
-import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
-import br.com.arquitetoandroid.appcommerce.ProductDetailActivity
 import br.com.arquitetoandroid.appcommerce.R
 import br.com.arquitetoandroid.appcommerce.model.OrderedProduct
-import br.com.arquitetoandroid.appcommerce.model.Product
 
 class CartAdapter(val list: List<OrderedProduct>, val context: Context): RecyclerView.Adapter<CartAdapter.ViewHolder>() {
 
     class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
         val image: ImageView = itemView.findViewById(R.id.iv_product_image)
         val title: TextView = itemView.findViewById(R.id.tv_product_title)
+        val color: TextView = itemView.findViewById(R.id.tv_color)
+        val size: TextView = itemView.findViewById(R.id.tv_size)
+        val price: TextView = itemView.findViewById(R.id.tv_price)
+        val quantity: TextView = itemView.findViewById(R.id.tv_qtd)
+        val qtdUp: ImageView = itemView.findViewById(R.id.iv_qtd_up)
+        val qtdDown: ImageView = itemView.findViewById(R.id.iv_qtd_down)
         val cardView: CardView = itemView.findViewById(R.id.cv_product_item)
     }
 
@@ -32,13 +36,33 @@ class CartAdapter(val list: List<OrderedProduct>, val context: Context): Recycle
 
     override fun onBindViewHolder(holder: CartAdapter.ViewHolder, position: Int) {
         val orderProduct: OrderedProduct = list[position]
-        holder.title.text = product.title
+        holder.title.text = orderProduct.product.title
         holder.image.setImageResource(R.drawable.camiseta_mockup)
-        holder.cardView.setOnClickListener {
-            val intent: Intent = Intent(context, ProductDetailActivity::class.java)
-            intent.putExtra("PRODUCT", product)
-            context.startActivity(intent)
+        holder.color.text = orderProduct.product.colors[0].name
+        holder.size.text = orderProduct.product.sizes[0].size
+        holder.quantity.text = orderProduct.quantity.toString()
+
+        holder.qtdUp.setOnClickListener {
+            orderProduct.quantity += 1
+            holder.quantity.text = orderProduct.quantity.toString()
+            updatePrice(holder, orderProduct)
         }
+
+        holder.qtdDown.setOnClickListener {
+            if (orderProduct.quantity > 0) {
+                orderProduct.quantity -= 1
+                holder.quantity.text = orderProduct.quantity.toString()
+                updatePrice(holder, orderProduct)
+            } else {
+                Toast.makeText(context, "Quantidade deve ser igual ou superior a zero", Toast.LENGTH_LONG).show()
+            }
+        }
+
+        updatePrice(holder, orderProduct)
+    }
+
+    private fun updatePrice(holder: ViewHolder, orderedProduct: OrderedProduct) {
+        holder.price.text = "R$ ${orderedProduct.product.price * orderedProduct.quantity}"
     }
 
 }
