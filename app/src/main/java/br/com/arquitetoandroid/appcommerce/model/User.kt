723a4fe37f3ae2f284a14921adc8e23470c722c3
Leonardo Leão de Assis <leonardo.leao.assis@gmail.com>
2021-03-15 20:19:30 -0300

    createUser

--- app/src/main/java/br/com/arquitetoandroid/appcommerce/model/User.kt
+++ app/src/main/java/br/com/arquitetoandroid/appcommerce/model/User.kt
@@ -1,6 +1,7 @@
 package br.com.arquitetoandroid.appcommerce.model
 
 import androidx.room.Entity
+import androidx.room.Ignore
 import androidx.room.PrimaryKey
 import java.io.Serializable
 import java.util.*
@@ -8,9 +9,13 @@ import java.util.*
 @Entity(tableName = "user_usr")
 data class User(
 
-    @PrimaryKey val id: String = UUID.randomUUID().toString(),
+    @PrimaryKey var id: String = UUID.randomUUID().toString(),
     var email: String,
     var name: String,
     var surname: String,
     var password: String,
-    var image: String): Serializable
+    var image: String): Serializable {
+
+    @Ignore constructor(): this("", "", "", "", "", "")
+
+}
--- app/src/main/java/br/com/arquitetoandroid/appcommerce/repository/UserRepository.kt
+++ app/src/main/java/br/com/arquitetoandroid/appcommerce/repository/UserRepository.kt
@@ -1,22 +1,60 @@
 package br.com.arquitetoandroid.appcommerce.repository
 
 import android.app.Application
+import android.util.Log
 import androidx.preference.PreferenceManager
 import br.com.arquitetoandroid.appcommerce.database.AppDatabase
 import br.com.arquitetoandroid.appcommerce.model.User
 import br.com.arquitetoandroid.appcommerce.model.UserAddress
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import com.google.firebase.firestore.FirebaseFirestore
+import org.json.JSONObject
 
 class UserRepository(val application: Application) {
 
     companion object {
-        val USER_ID = "USER_ID"
+        private val TAG = UserRepository::class.java.simpleName
+
+        private val USER_ID = "USER_ID"
+
+        const val BASE_URL = "https://identitytoolkit.googleapis.com/v1/"
+        const val SIGNUP = "accounts:signUp"
+        const val SIGNIN = "accounts:signInWithPassword"
+        const val PASSWORD_RESET = "accounts:sendOobCode"
+        const val KEY = "?key=AIzaSyDQAMjXwmEy3Xg-JB7gCG7_s4CtDO-f5j8"
     }
 
     private val database = AppDatabase.getDatabase(application)
 
     private val userDao = database.userDao()
 
+    private val firestore = FirebaseFirestore.getInstance()
+    private val queue = Volley.newRequestQueue(application)
+
     fun login(email: String, password: String) = userDao.login(email, password)
+
+    fun createUser(user: User) {
+        val params = JSONObject().also {
+            it.put("email", user.email)
+            it.put("password", user.password)
+            it.put("returnSecureToken", true)
+        }
+
+        val request = JsonObjectRequest(Request.Method.POST, BASE_URL + SIGNUP + KEY, params,
+            { response ->
+                user.id = response.getString("localId")
+                user.password = response.getString("idToken")
+
+                firestore.collection("user_usr").document(user.id).set(user)
+            },
+            { error ->
+                Log.e(TAG, error.message ?: "Error")
+            })
+    }
+
     fun loadWithAddresses(userId: String) = userDao.loadUserById(userId)
     fun insert(user: User) = userDao.insert(user)
     fun insert(userAddress: UserAddress) = userDao.insert(userAddress)
